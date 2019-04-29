60b2ba30e03b3ec64f49762f6c8fbd0b191fbf55
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-04-29 16:24:04 -0500

    Add readline module to get user input.
    Realized once I began the processing user input that this app
    would be really time consuming with multiple users, so I'm gonna
    refactor for now to just display MY email, calendar, etc, and leave
    out the login and create user stuff.

--- app.js
+++ app.js
@@ -1,19 +1,47 @@
 /* 
 
-Create a login menu with two options:
-
-1. Login
-2. Create User Account
+Get the user's response to the login menu
 
 */
 
 // laziness...
 const l = console.log
 
-const displayLoginMenu = () => {
+// to get user input, we use the node module 'readline'
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'PLANNER> '
+});
+
+displayLoginMenu()
+
+rl.prompt();
+
+rl.on('line', (line) => {
+
+  
+
+  switch (line.trim()) {
+    case 'hello':
+      console.log('world!');
+      break;
+    default:
+      console.log(`Say what? I might have heard '${line.trim()}'`);
+      break;
+  }
+  rl.prompt();
+}).on('close', () => {
+  console.log('Have a great day!');
+  process.exit(0);
+});
+
+function displayLoginMenu() {
   l('*** Please Choose An Option: ***')
   l('| 1. Login')
   l('| 2. Create User Account')
 }
 
-displayLoginMenu()
+
