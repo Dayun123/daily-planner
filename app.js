419d38b800b89b31c1ca54a61ecdf0885824e9b2
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-04-29 16:35:04 -0500

    Add ability to get user's response.
    Used the readline module to capture the user's response and display
    a simple error message if the response is not valid.

--- app.js
+++ app.js
@@ -10,8 +10,36 @@ Display the options to the user like so:
 
 // laziness...
 const l = console.log
+// need readline to process user input
+const readline = require('readline');
 
 displayLoginMenu()
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'PLANNER> '
+});
+
+rl.prompt();
+
+rl.on('line', (line) => {
+  const option = line.trim()
+  
+  // there are two options, '1', and '2'
+  if (option === '1') {
+    l('Option 1 selected')
+  } else if (option === '2') {
+    l('Option 2 selected')
+  } else {
+    l(`${option} is not a valid option!`)
+  }
+  rl.prompt();
+}).on('close', () => {
+  console.log('Have a great day!');
+  process.exit(0);
+});
+
 
 function displayLoginMenu() {
   l('*** What would you like to see? ***')
