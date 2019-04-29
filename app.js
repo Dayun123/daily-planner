b9817bc829efb84abb3050ce5dd0236a60695c7b
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-04-29 16:10:33 -0500

    Create app.js file and add display login menu functionality.
    Created a simple function that displays the login menu once the app is started.

---
+++ app.js
@@ -0,0 +1,19 @@
+/* 
+
+Create a login menu with two options:
+
+1. Login
+2. Create User Account
+
+*/
+
+// laziness...
+const l = console.log
+
+const displayLoginMenu = () => {
+  l('*** Please Choose An Option: ***')
+  l('| 1. Login')
+  l('| 2. Create User Account')
+}
+
+displayLoginMenu()
